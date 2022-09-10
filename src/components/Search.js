8dc85f01b24a9333fab25cf978b9ce59d70932fa
kit <tingiao.ki@ttpsc.com>
2022-09-10 13:59:56 +0800

    Finsish Accordion and Search widgets.

---
+++ src/components/Search.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    const [ term, setTerm ] = useState('programming');
+    const [ debounceTerm, setDebounceTerm ] = useState(term);
+    const [ results, setResults ] = useState([]);
+
+    console.log(results);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebounceTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debounceTerm
+                }
+            });
+
+            setResults(data.query.search);
+        }
+
+        search();
+
+    }, [debounceTerm]);
+
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className='item' key={ result.pageid }>
+                <div className='right floated content'>
+                    <a className='ui button' href={`https://en.wikipedia.org?curid=${ result.pageid }`}>
+                        Go
+                    </a>
+                </div>
+                <div className='content'>
+                    <div className='header'>
+                        { result.title }
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );;
+    })
+
+    return (
+        <div>
+            <div className='ui form'>
+                <div className='field'>
+                    <label>Enter Search Term</label>
+                    <input className='input' value={ term } onChange={ (event) => setTerm(event.target.value) } />
+                </div>
+            </div>
+            <div className='ui celled list'>
+                { renderedResults }
+            </div>
+        </div>
+    );
+}
+
+export default Search;
